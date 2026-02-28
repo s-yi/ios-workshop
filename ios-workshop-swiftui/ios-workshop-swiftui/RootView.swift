485dffdc2902e300060995fabd1850e70526099f
Samuel Yi <samuel.yi@wolt.com>
2026-02-28 02:30:48 +0100

    added base swiftui project

---
+++ ios-workshop-swiftui/ios-workshop-swiftui/RootView.swift
@@ -0,0 +1,22 @@
+//
+//  RootView.swift
+//  Copyright © 2026 DoorDash. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RootView()
+}
---
+++ ios-workshop-swiftui/ios-workshop-swiftui/ios_workshop_swiftuiApp.swift
@@ -0,0 +1,15 @@
+//
+//  ios_workshop_swiftuiApp.swift
+//  Copyright © 2026 DoorDash. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ios_workshop_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
