af7f3714f0424c3041f4a8d2ee1c2d95612aca53
Samuel Yi <samuel.yi@wolt.com>
2026-03-02 10:05:47 +0100

    add lifecycle methods to vc

--- ios-workshop-uikit/ios-workshop-uikit/RootViewController.swift
+++ ios-workshop-uikit/ios-workshop-uikit/RootViewController.swift
@@ -7,12 +7,45 @@ import UIKit
 
 class RootViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        print("RootViewController - init")
     }
 
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        print("RootViewController - init(coder:)")
+    }
+
+    deinit {
+        print("RootViewController - deinit")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        print("RootViewController - viewDidLoad")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("RootViewController - viewWillAppear")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("RootViewController - viewDidAppear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("RootViewController - viewWillDisappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("RootViewController - viewDidDisappear")
+    }
 
 }
 
